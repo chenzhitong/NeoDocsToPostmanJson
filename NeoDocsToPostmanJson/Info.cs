882bea87b7cf086336e82f2d95b68c06acc3ba06
陈志同 <bitcoin2077@outlook.com>
2024-08-27 14:07:37 +0800

    添加项目文件。

---
+++ NeoDocsToPostmanJson/Info.cs
@@ -0,0 +1,63 @@
+﻿namespace NeoDocsToPostmanJson
+{
+    internal class Export
+    {
+        public Info Info { get; set; }
+
+        public List<Item> Item { get; set; } = new List<Item>();
+    }
+
+    internal class Info
+    {
+        public string _postman_id = "9c0b66ec-bc1a-4b47-8858-9a82ed88cb9d";
+
+        public string Name = "RpcServer";
+
+        public string Schema = "https://schema.getpostman.com/json/collection/v2.1.0/collection.json";
+
+        public string _exporter_id = "1641087";
+
+        public string PowerBy = "NeoDocsToPostmanJson";
+
+        public string GitHub = "https://github.com/chenzhitong/NeoDocsToPostmanJson";
+    }
+
+    internal class Item
+    {
+        public string Name { get; set; }
+
+        public Request Request { get; set; } = new Request();
+
+        public Array Response { get; set; } = Array.Empty<string>();
+    }
+
+    internal class Request
+    {
+        public string Method = "POST";
+
+        public Array Header { get; set; } = Array.Empty<string>();
+
+        public Body Body { get; set; } = new Body();
+
+        public string Description { get; set; }
+    }
+
+    internal class Body
+    {
+        public string Mode = "raw";
+
+        public string Raw = "raw";
+
+        public Options Options { get; set; } = new Options();
+    }
+
+    internal class Options
+    {
+        public Raw Raw { get; set; } = new Raw();
+
+    }
+    internal class Raw
+    {
+        public string Language = "json";
+    }
+}
---
+++ NeoDocsToPostmanJson/Program.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Toolkit.Parsers.Markdown;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.IO;
+using System.Reflection.Emit;
+
+namespace NeoDocsToPostmanJson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var defaultPath = "D:\\1Code\\chen\\neo-dev-portal\\docs\\n3\\reference\\rpc";
+            if (!Directory.Exists(defaultPath))
+            {
+                Console.WriteLine("Please download neo-dev-portal project https://github.com/neo-project/neo-dev-portal\r\nand input the path of neo RPC docs, such as: D:\\neo-dev-portal\\n3\\reference\\rpc");
+                defaultPath = Console.ReadLine();
+            }
+
+            var files = Directory.GetFiles(defaultPath, "*.md").ToList();
+            files.RemoveAll(p => p.Contains("api.md"));
+
+            var export = new Export();
+
+            foreach (var file in files)
+            {
+                var document = new MarkdownDocument();
+                document.Parse(File.ReadAllText(file));
+                var title = document.Blocks.FirstOrDefault(p => p.Type == MarkdownBlockType.Header)?.ToString().Trim();
+                var name = title.Split(' ')[0];
+                var raw = document.Blocks.FirstOrDefault(p => p.Type == MarkdownBlockType.Code)?.ToString().Trim();
+                var desc = "";
+                foreach (var item in document.Blocks.Skip(1))
+                {
+                    if (item.ToString().Contains("Example")) break;
+                    desc += item.ToString();
+                }
+                desc = desc.Replace(":::", string.Empty);
+                export.Info = new Info();
+                export.Item.Add(new Item()
+                {
+                    Name = name,
+                    Request = new Request()
+                    {
+                        Body = new Body()
+                        {
+                            Raw = raw
+                        },
+                        Description = desc
+                    }
+                });
+            }
+
+            var json = JsonConvert.SerializeObject(export, new JsonSerializerSettings{
+                ContractResolver = new LowercaseContractResolver(),
+                Formatting = Formatting.Indented
+            });
+            Console.WriteLine(json);
+            var outputFileName = "RpcServer.postman_collection.json";
+            File.WriteAllText(outputFileName, json);
+            Console.WriteLine($"Save to {outputFileName}");
+            Console.ReadLine();
+        }
+    }
+    public class LowercaseContractResolver : DefaultContractResolver
+    {
+        protected override string ResolvePropertyName(string propertyName)
+        {
+            return propertyName.ToLower();
+        }
+    }
+}
